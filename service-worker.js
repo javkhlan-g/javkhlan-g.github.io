5b9657ede4437ee31b925944c1172ded760aa47d
jack <javxlan@gmail.com>
2021-03-10 00:54:37 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fca6720d2c51b7578a32adac456cc17.js"
+  "/precache-manifest.0f2cc67494d245239031fb494dc4a137.js"
 );
 
 self.addEventListener('message', (event) => {
